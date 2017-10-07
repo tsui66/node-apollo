b32242cb98026a2b5fba414582fcc307d1286fd7
Quinton.Xu <quinton.xu@gmail.com>
2017-10-07 14:03:39 +0800

    fix: unit test

--- README.md
+++ README.md
@@ -10,7 +10,10 @@ $ npm i node-apollo --save-dev
 
 ## Usage
 
-see test example
+* 创建namesapce
+* [获取Apollo token](https://github.com/ctripcorp/apollo/wiki/Apollo%E5%BC%80%E6%94%BE%E5%B9%B3%E5%8F%B0)
+* see test example
+* 目前只支持JSON格式
 
 ## License
 
--- test/index.js
+++ test/index.js
@@ -11,9 +11,9 @@ describe('#index', function () {
       clusterName: 'default',
       namespaceName: '',
       apolloEnv: 'dev',
+      token: '<apollo access token>'
       // clientIp: '',
     };
-    const token = '<apollo access token>';
     const result = await apollo.remoteConfigService(token, config);
     assert(Object.keys(result).length > 0, 'Read config failed');
   });
