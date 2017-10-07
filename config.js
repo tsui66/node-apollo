bca5a6e660a587473f48f5255272bd16acda291b
Quinton.Xu <quinton.xu@gmail.com>
2017-10-07 17:32:47 +0800

    feat: read config from env

---
+++ config.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports = {
+  ENV_FILE_PATH: __dirname + '/default.env',
+};
--- test/index.js
+++ test/index.js
@@ -1,11 +1,13 @@
 'use strict';
 
+const fs = require('fs');
 const assert = require('assert');
 const apollo = require('../index.js');
+const config = require('../config.js');
 
 describe('#index', function () {
   it('index.remoteConfigService', async () => {
-    const config = {
+    const eggConfig = {
       configServerUrl: 'http://example.com',
       appId: '<appId>',
       clusterName: 'default',
@@ -14,7 +16,24 @@ describe('#index', function () {
       token: '<apollo access token>'
       // clientIp: '',
     };
-    const result = await apollo.remoteConfigService(token, config);
-    assert(Object.keys(result).length > 0, 'Read config failed');
+    // const result = await apollo.remoteConfigService(config);
+    // assert(Object.keys(result).length > 0, 'Read config failed');
+  });
+  it('index.createEnvFile', async () => {
+    const eggConfig = {
+      configServerUrl: 'http://example.com',
+      appId: '<appId>',
+      clusterName: 'default',
+      namespaceName: '',
+      apolloEnv: 'dev',
+      token: '<apollo access token>'
+      // clientIp: '',
+    };
+    apollo.createEnvFile(eggConfig);
+    assert(fs.existsSync(config.ENV_FILE_PATH), 'create env file failed');
+  });
+  it('index.setEnv', async () => {
+    apollo.setEnv();
+    assert(process.env.configServerUrl, 'Read config failed');
   });
 });
